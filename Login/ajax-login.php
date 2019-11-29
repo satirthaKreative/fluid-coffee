62f561d2f6d0212b32f537c9b1feb8a358935ae0
satirtha <satirtha.kreative@gmail.com>
2019-11-29 16:33:24 +0530

    rest part

---
+++ Login/ajax-login.php
@@ -0,0 +1,19 @@
+<?php 
+
+	require_once '../db/db.php';
+	// data set
+	$errmsg['no_error'] = false;
+	// my queries
+	$data1 = $_POST['username'];
+	$data2 = md5($_POST['pass']);
+	//checking query
+	$check_data =  $my_db->query("SELECT * FROM `qa_users` WHERE email = '".$data1."' AND handle = '".$data2."' ");
+	$fetch_data = $check_data->fetch_array();
+	// count rows
+	if($check_data->num_rows>0)
+	{
+		$_SESSION['log_user_id'] = $fetch_data['userid'];
+		$errmsg['no_error'] = true;
+	}
+	echo json_encode($errmsg);
+?>
---
+++ db/db.php
@@ -0,0 +1,5 @@
+<?php  
+  $my_db = new mysqli('localhost','root','','coffe');
+  session_start();
+  $db_prefix = 'qa_campaign_';
+?>
