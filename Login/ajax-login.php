de855bd68f33d8dfe07148ff03bab1fac3d498e4
satirtha <satirtha.kreative@gmail.com>
2019-11-29 17:53:58 +0530

    Update on 29th Nov,2019

--- Login/ajax-login.php
+++ Login/ajax-login.php
@@ -7,13 +7,14 @@
 	$data1 = $_POST['username'];
 	$data2 = md5($_POST['pass']);
 	//checking query
-	$check_data =  $my_db->query("SELECT * FROM `qa_users` WHERE email = '".$data1."' AND handle = '".$data2."' ");
+	$check_data =  $my_db->query("SELECT * FROM `qa_users` WHERE email = '".$data1."' AND pass_view = '".$data2."' ");
 	$fetch_data = $check_data->fetch_array();
 	// count rows
 	if($check_data->num_rows>0)
 	{
 		$_SESSION['log_user_id'] = $fetch_data['userid'];
 		$errmsg['no_error'] = true;
+		$errmsg['mainmsg'] = "Successfully Logged In! Redirecting....";
 	}
 	echo json_encode($errmsg);
 ?>
